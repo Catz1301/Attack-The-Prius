865c9b90350a470509436fda32f2df060eba7757
Whiskers <33127788+Catz1301@users.noreply.github.com>
2020-03-12 08:37:13 -0400

    Update ElonBullet.cpp

--- ElonBullet.cpp
+++ ElonBullet.cpp
@@ -15,12 +15,23 @@ ElonBullet::~ElonBullet()
 
 ElonBullet::update(float &dt)
 {
-	// TODO: Shrink scaleSize
 	scaleSize -= sf::Vector2f(0.05, 0.05);
 	size = sf::Vector2f(elonBullet.getGlobalBounds().width, elonBullet.getGlobalBounds().height);
-	if (scaleSize.x < 0.1 && scaleSize.y < 0.1) {
-		// TODO: Remove elonBullet and prius it shot.
-	}
+}
+
+ElonBullet::readyToDie()
+{
+	return (scaleSize.x < 0.1 && scaleSize.y < 0.1);
+}
+
+ElonBullet::getScaleSize()
+{
+	return scaleSize;
+}
+
+ElonBullet::getTargetPoint()
+{
+	return elonBullet.getPosition();
 }
 
 ElonBullet::draw(sf::RenderWindow &target)
